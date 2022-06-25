9df94f209236346abd6f356b13d796944356ea8d
Zee <l.v.d.zee@student.tue.nl>
2022-06-25 18:38:22 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,173 @@
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+//import "./style.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation, Pagination, Mousewheel, Keyboard],
+    };
+  },
+};
+</script>
+
+<template>
+<nav class="navbar">
+  <div class="container">
+    <div id="navMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Home
+        </a>
+        <a class="navbar-item">
+          Documentation
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-dark">Github</a>
+            <a class="button is-link">Download</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<section class="hero is-link is-fullheight-with-navbar">
+  <div class="hero-body">
+    <p class="title">
+      Fullheight hero with navbar
+    </p>
+  </div>
+</section>
+
+<section class="section is-medium">
+  <h1 class="title">Academics</h1>
+  <div class="tile is-ancestor">
+    <swiper
+      :cssMode="true"
+      :navigation="true"
+      :pagination="true"
+      :mousewheel="true"
+      :keyboard="true"
+      :modules="modules"
+      class="mySwiper hero-body section is-medium tile is-9"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+    </swiper>
+
+  </div>
+</section>
+
+<section class="hero is-danger is-fullheight">
+  <div class="hero-body">
+    <div class="">
+      <p class="title">
+        Fullheight hero
+      </p>
+      <p class="subtitle">
+        Fullheight subtitle
+      </p>
+    </div>
+  </div>
+</section>
+
+<section class="section is-large">
+  <h1 class="title">Large section</h1>
+  <h2 class="subtitle">
+    A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
+  </h2>
+</section>
+
+<h1 class="title">
+  Bulma
+  </h1>
+
+  <p class="subtitle">
+    Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+  </p>
+
+  <div class="field">
+    <div class="control">
+      <input class="input" type="text" placeholder="Input">
+    </div>
+  </div>
+
+  <div class="field">
+    <p class="control">
+      <span class="select">
+        <select>
+          <option>Select dropdown</option>
+        </select>
+      </span>
+    </p>
+  </div>
+
+  <div class="buttons">
+    <a class="button is-primary">Primary</a>
+    <a class="button is-link">Link</a>
+  </div>
+  <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+    </p>
+  </div>
+</footer>
+</template>
+
+<style>
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
